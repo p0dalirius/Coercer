9c93c083613f3d5507e342fdd60c6ad3da5f3981
p0dalirius <podalirius@protonmail.com>
2022-07-09 23:36:45 +0200

    Release 1.4: Added MS-RPRN 'PrinterBug'

--- coercer.py
+++ coercer.py
@@ -13,7 +13,7 @@ from lib.protocols import MS_EFSR, MS_FSRVP, MS_DFSNM, MS_RPRN
 from lib.utils.smb import connect_to_pipe, can_bind_to_protocol, get_available_pipes_and_protocols
 
 
-VERSION = "1.3"
+VERSION = "1.4"
 
 banner = """
        ______                              
